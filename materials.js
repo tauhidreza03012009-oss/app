3c904df954e0be26dea80fc539175eb8ed93e41c
tauhidreza03012009-oss <tauhidreza03012009@gmail.com>
2026-06-11 17:50:14 +0600

    Create materials.js

---
+++ materials.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+
+export const grassMat  = new THREE.MeshStandardMaterial({color: 0x4c7c4c, roughness: 0.9});
+export const pathMat   = new THREE.MeshStandardMaterial({color: 0xdfc49f, roughness: 0.8});
+export const woodMat   = new THREE.MeshStandardMaterial({color: 0x6f4e37, roughness: 0.7});
+export const roofMat   = new THREE.MeshStandardMaterial({color: 0xb23b3b, roughness: 0.6});
+export const leafMat   = new THREE.MeshStandardMaterial({color: 0x2e5c2e, roughness: 0.9});
+export const stoneMat  = new THREE.MeshStandardMaterial({color: 0x7a828a, roughness: 0.6});
+export const crateMat  = new THREE.MeshStandardMaterial({color: 0xc69c6d, roughness: 0.5});
+export const borderMat = new THREE.MeshStandardMaterial({color: 0x2b3e2b, roughness: 0.9});
