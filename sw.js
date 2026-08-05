3d931e7e150e4f3623bf82444324304533db924e
tauhidreza03012009-oss <tauhidreza03012009@gmail.com>
2026-08-05 16:48:07 +0600

    Update instalation

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
 const CACHE_NAME = 'v1_site_cache';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/style.css',
+  './',
+  './index.html',
+  './style.css',
+  './icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Service Worker: Caching Files');
       return cache.addAll(ASSETS_TO_CACHE);
     })
   );
 });
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log('Service Worker: Clearing Old Cache');
             return caches.delete(cache);
           }
         })
